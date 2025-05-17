83a0cdaebeba98909c39080e2e517e908efbc20c
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-17 05:11:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (41.82%)

---
+++ Submissions/0035-search-insert-position/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+
+    int lb(vector<int> &nums, int target){
+        int n = nums.size();
+        int ans = n;
+        int low=0,high=n-1;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            if(nums[mid] >= target){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        return ans;
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+        int ans = lb(nums,target);
+        return ans;
+    }
+};
