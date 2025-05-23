588cc4e27fae2c9841ae4c63dfe12825198582eb
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-23 05:54:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.6 MB (95.01%)

---
+++ Submissions/0232-implement-queue-using-stacks/solution.cpp
@@ -0,0 +1,43 @@
+class MyQueue {
+public:
+    stack<int> ip,op;
+
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        ip.push(x);
+    }
+    
+    int pop() {
+        peek();
+        int val = op.top();
+        op.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(op.empty()){
+            while(!ip.empty()){
+                op.push(ip.top());
+                ip.pop();
+            }
+        }
+
+        return op.top();
+    }
+    
+    bool empty() {
+        return ip.empty() && op.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
