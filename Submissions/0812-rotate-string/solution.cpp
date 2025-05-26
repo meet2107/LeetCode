30f139ceb35ca8162fd3e729993901f55d9ff9d2
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-26 08:33:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (61.01%)

---
+++ Submissions/0812-rotate-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+
+        if((s+s).find(goal) == string::npos){
+            return false;
+        }
+
+        return true;
+    }
+};
