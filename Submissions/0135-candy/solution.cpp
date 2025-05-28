4ce36af64a0c7a4f3bbb9e6268b55cd9e4563210
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-28 10:43:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.8 MB (99.43%)

---
+++ Submissions/0135-candy/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int  n = ratings.size();
+        int candy = n;
+        int i=1;
+
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
+            }
+
+            int peak = 0;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                candy += peak;
+                i++;
+            }
+
+            if(i==n){
+                return candy;
+            }
+
+            int down = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                down++;
+                candy += down;
+                i++;
+            }
+
+            candy -= min(peak,down);
+        }
+
+        return candy;
+    }
+};
