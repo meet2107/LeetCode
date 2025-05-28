4cde6b80b519f4ede1588b6b1db7810a431650e4
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-28 08:16:43 +0000

    [LeetCode Sync] Runtime - 3 ms (12.10%), Memory - 87.2 MB (39.29%)

---
+++ Submissions/0890-lemonade-change/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+        int five=0,ten=0;
+
+        for(int i=0;i<n;i++){
+            if(bills[i] == 5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                if(five == 0){
+                    return false;
+                }
+                five--;
+                ten++;
+            }
+            else{
+                if(ten && five){
+                    ten--;
+                    five--;
+                }
+                else if(five >= 3){
+                    five -= 3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
