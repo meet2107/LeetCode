22e12cdae2581f1bec3e57f04cf10974c1965613
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-05-29 06:49:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (58.70%)

---
+++ Submissions/0136-single-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xorr = 0;
+        for(int i=0;i<n;i++){
+            xorr ^= nums[i];
+        }
+
+        return xorr;
+    }
+};
