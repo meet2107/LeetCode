7c08ded73d6c4e2566ac1271c1df00e39fa9ed0a
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-06-01 03:04:04 +0000

    [LeetCode Sync] Runtime - 75 ms (22.22%), Memory - 49.9 MB (11.11%)

---
+++ Submissions/3884-minimum-absolute-difference-in-sliding-submatrix/solution.cpp
@@ -0,0 +1,88 @@
+  /*
+███╗░░░███╗███████╗███████╗████████╗
+████╗░████║██╔════╝██╔════╝╚══██╔══╝
+██╔████╔██║█████╗░░█████╗░░░░░██║░░░
+██║╚██╔╝██║██╔══╝░░██╔══╝░░░░░██║░░░
+██║░╚═╝░██║███████╗███████╗░░░██║░░░
+╚═╝░░░░░╚═╝╚══════╝╚══════╝░░░╚═╝░░░ */
+    
+ 
+#include<bits/stdc++.h>
+#include<fstream>
+#include<cstdio>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<cmath>
+#include<math.h>
+using namespace std;
+#define Meet_Andharia ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define lld long double
+#define pii pair<int,int>
+#define pb push_back
+#define vi vector<int>
+#define vll vector<long long int>
+#define fori1(l,n) for(int i=1;i<n;i++)
+#define fori(o,n) for(ll i=0;i<n;i++)
+#define forii(l,r) for(ll i=l;i<r;i++)
+#define forimn(m,n) for(int i=m;i<=n;i++)
+#define forj(m,n) for(int j=m;j<n;j++)
+#define forir(m,n) for(int i=n-1;i>=m;i--)
+#define forjr(m,n) for(int j=n-1;j>=m;j--)
+#define forje(m,n) for(int j=m;j<=n;j++)
+#define fork(m,n) for(int k=m;k<n;k++)
+#define forl(m,n) for(int l=m;l<n;l++)
+#define loopi(v) for(auto i:v)
+#define fi first
+#define se second
+#define in(n) int n;cin>>n;
+#define inll(n) ll n;cin>>n;
+#define instr(n) string n;cin>>n;
+#define inarr(arr,n) long long int arr[n];for(int i=0;i<n;i++)cin>>arr[i];
+#define outarr(arr,n) for(int i=0;i<n;i++){cout<<arr[i]<<" ";}cout<<endl;
+#define pyes cout<<"YES\n";
+#define pno cout<<"NO\n";
+#define pneg1 cout<<"-1\n";
+#define p0 cout<<"0\n";
+#define CASE(x) cout<<"Case #"<<x<<": ";
+#define all(v)  v.begin(),v.end()
+
+const ll MOD = 1000000007LL;
+const ll MOD2= 998244353;
+const ll inf=1e18;
+
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        ll m = grid.size();
+        ll n = grid[0].size();
+
+        vector<vector<int>> ans(m-k+1,vector<int>(n-k+1));
+
+        forimn(0,m-k){
+            for(ll j=0;j<=n-k;j++){
+                set<ll> s;
+                for(ll l=i;l<i+k;l++){
+                    for(ll r=j;r<j+k;r++){
+                        s.insert(grid[l][r]);
+                    }
+                }
+
+                if(s.size()>1){
+                    vll t(all(s));
+                    ll dif = INT_MAX;
+                    fori1(1,t.size()){
+                        dif = min(dif,t[i]-t[i-1]);
+                    }
+                    ans[i][j] = dif;
+                }
+                else{
+                    ans[i][j] = 0;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
