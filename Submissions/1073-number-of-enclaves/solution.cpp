7bc6ca011a64f9a1e49eb0becec05b22f5c05af4
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-06-01 06:41:41 +0000

    [LeetCode Sync] Runtime - 11 ms (44.04%), Memory - 38.8 MB (20.54%)

---
+++ Submissions/1073-number-of-enclaves/solution.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    int dx[4] = {0,0,-1,1};
+    int dy[4] = {-1,1,0,0};
+    int m,n;
+
+    void dfs(int r, int c, vector<vector<int>> &vis, vector<vector<int>> &board){
+        vis[r][c] = 1;
+
+        for(int i=0;i<4;i++){
+            int nr = r+dx[i];
+            int nc = c+dy[i];
+
+            if(nr>=0 && nr<m && nc>=0 && nc<n && board[nr][nc] == 1 && !vis[nr][nc]){
+                dfs(nr,nc,vis,board);
+            }
+        }
+    }
+
+    int numEnclaves(vector<vector<int>>& board) {
+        m = board.size();
+        n = board[0].size();
+
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        for(int j=0; j<n; j++){
+            if(!vis[0][j] && board[0][j] == 1){
+                dfs(0, j, vis, board);
+            }
+            if(!vis[m-1][j] && board[m-1][j] == 1){
+                dfs(m-1, j, vis, board);
+            }
+        }
+        
+        for(int i=0; i<m; i++){
+            if(!vis[i][0] && board[i][0] == 1){
+                dfs(i, 0, vis, board);
+            }
+            if(!vis[i][n-1] && board[i][n-1] == 1){
+                dfs(i, n-1, vis, board);
+            }
+        }
+
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j] && board[i][j] == 1){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
