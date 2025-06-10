1d7529741eec3d2d859509d48fbabf8d9c32ee55
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-06-10 06:48:57 +0000

    [LeetCode Sync] Runtime - 7 ms (96.95%), Memory - 9 MB (86.84%)

---
+++ Submissions/0115-distinct-subsequences/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int f(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(j<0){
+            return 1; // all the elements of string t matched
+        }
+        if(i<0){
+            return 0; // string s exhausted and string t is not
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s[i] == t[j]){
+            return dp[i][j] = f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp);
+        }
+        return dp[i][j] = f(i-1,j,s,t,dp);
+    }
+
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        vector<double> prev(m+1,0);
+        prev[0] = 1; // meaning that t is exhausted => all matched
+
+        for(int i=1;i<=n;i++){
+            for(int j=m;j>=1;j--){
+                if(s[i-1] == t[j-1]){
+                    prev[j] = prev[j-1] + prev[j];
+                }
+            }
+        }
+
+        return (int)prev[m];
+    }
+};
