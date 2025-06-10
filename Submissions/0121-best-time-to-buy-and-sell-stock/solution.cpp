cfa704ca3f7f6f5d3abf8a09e5e66c152b6c9f59
Meet Andharia <134314590+meet2107@users.noreply.github.com>
2025-06-10 12:16:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 97.4 MB (60.98%)

---
+++ Submissions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int pro = 0;
+        int mini = prices[0];
+
+        for(int i=1;i<n;i++){
+            pro = max(pro,prices[i]-mini);
+            mini = min(mini,prices[i]);
+        }
+
+        return pro;
+    }
+};
